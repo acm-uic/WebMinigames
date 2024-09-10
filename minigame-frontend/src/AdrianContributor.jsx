2feaae473a0e931612dd80ab6f141998d72ccf1c
Ajknight121 <aknig2@uic.edu>
2024-09-09 23:10:58 -0500

    Remove template style, added tailwindcss

--- minigame-frontend/src/AdrianContributor.jsx
+++ minigame-frontend/src/AdrianContributor.jsx
@@ -1,13 +1,13 @@
 import AdrianProfileImg from './Arrow Swarm trails.png'
 
 
-export function AdrianContributor(props) {
+export function AdrianContributor() {
   return (
-    <div className='contributor-adrian'>
+    <div className='contributor-adrian flex bg-slate-500 w-[300px] p-1'>
         <img src={AdrianProfileImg} width={"100px"}/>
-        <div>
-          {props.name}
-          {props.username ? props.username : " No username"}
+        <div className='text-white text-center justify-center m-auto'>
+          <div>Adrian Knight</div>
+          <div>@Ajknight121</div>
         </div>
       </div>
   )
