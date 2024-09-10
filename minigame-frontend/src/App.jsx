667650912682259e7f7147bf7997595839663e5e
Ajknight121 <aknig2@uic.edu>
2024-09-09 23:10:58 -0500

    Remove template style, added tailwindcss

--- minigame-frontend/src/App.jsx
+++ minigame-frontend/src/App.jsx
@@ -1,44 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import AdrianProfileImg from './Arrow Swarm trails.png'
 import { AdrianContributor } from './AdrianContributor'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <h1 className='text-3xl'>Contributors</h1>
+      <h4>Add your contributor card below!</h4>
+      Default styling on HTML elements (h1,li) removed due to tailwindcss has been added
+      <div className='contributors m-10 p-3 gap-4 flex flex-wrap justify-center border border-solid border-gray-950'>
+        <AdrianContributor />
       </div>
-      <h1>Vite + React</h1>
-      <div className='contributor-adrian'>
-        <img src={AdrianProfileImg} width={"100px"}/>
-        <div>
-          Adrian Knight
-          @Ajknight121
-        </div>
-      </div>
-      <AdrianContributor name={"John"} username="@Johnmail"/>
-      <AdrianContributor name={"Jane"}/>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
