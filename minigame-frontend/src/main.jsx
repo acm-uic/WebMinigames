d442ee24969da240736864a4e266c50266a10b90
Ajknight121 <aknig2@uic.edu>
2024-10-07 15:51:13 -0500

    Installed react-router-dom and create /home page

--- minigame-frontend/src/main.jsx
+++ minigame-frontend/src/main.jsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+
 import App from './App.jsx'
 import './index.css'
+import { Homepage } from './page/Homepage.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+  {
+    path: "/home",
+    element: <Homepage />
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
