5fa85ec6d6f9027ecff5b3ef4623bcae1d5fc3b4
coalwork <cachedsid@gmail.com>
2024-10-07 16:32:09 -0500

    fleshing out the layout

--- minigame-frontend/src/page/Profile.jsx
+++ minigame-frontend/src/page/Profile.jsx
@@ -2,12 +2,24 @@ import React from 'react'
 
 export const Profile = () => {
   return (
-    <div id="user-container">
-      <img src="" alt="profile picture" />
-      <h1 id="user-name">placeholder</h1>
-      <h2 id="user-bio-header">about me</h2>
-      <p id="user-header">Lorem ipsum</p>
+    <div id="profile-page">
+      <div id="user-container">
+        <img src="" alt="profile picture" />
+        <div id="user-name-container">
+          <h1 id="user-name">placeholder</h1>
+          <button id="edit"></button>
+        </div>
+        <div id="user-bio-container">
+          <h2 id="user-bio-header">about me</h2>
+          <p id="user-bio">Lorem ipsum</p>
+        </div>
+      </div>
+      <div id="user-favorites">
+        // will hold Game components
+      </div>
+      <div id="user-rankings">
+        // will hold Ranking components
+      </div>
     </div>
   );
 }
-
