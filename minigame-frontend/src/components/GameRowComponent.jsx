d0015d7b601ce8e3ade395878b825dbef19b9967
Ajknight121 <aknig2@uic.edu>
2024-10-21 15:58:51 -0500

    Full height game image

--- minigame-frontend/src/components/GameRowComponent.jsx
+++ minigame-frontend/src/components/GameRowComponent.jsx
@@ -6,8 +6,8 @@ export function GameRowComponent(props) {
             <div className='text-[24px] w-full justify-start'>{props.GameName}</div>
             <div className='text-[12x] w-full justify-end'>{props.GameDesc}</div>
           </div>
-          <div className='w-[275px] bg-[#534444] h-[200px] flex-none justify-end p-2'>
-            <img src={props.GameImage} alt="" />
+          <div className='w-[275px] bg-[#534444] h-[200px] flex flex-none justify-center p-2'>
+            <img className="w-auto h-full" src={props.GameImage} alt="" />
           </div>
           <div className='bg-[#C7BFBF] h-[100px] w-[50px] flex-'></div>
         </div>
