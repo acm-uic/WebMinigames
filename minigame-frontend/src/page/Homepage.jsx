c53e2d6be5066376a7e39c5caa8a68b2d06b3fd2
dstyka2 <dstyka2@uic.edu>
2024-10-20 19:22:00 -0500

    GameRow component and props to test it

--- minigame-frontend/src/page/Homepage.jsx
+++ minigame-frontend/src/page/Homepage.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { GameRowComponent } from "../GameRowComponent.jsx";
 
 export const Homepage = () => {
   return (
-    <div>Hello Homepage</div>
+    <div>
+      <ol >
+      <GameRowComponent GameName="Test Game Name" GameDesc="Lorem Ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et. Lorem Ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et." GameImage="https://steamuserimages-a.akamaihd.net/ugc/273968969351308719/A642CC447714B0B1F2F927C1738A9C262308E7B0/?imw=128&imh=128&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true"/>
+      </ol>
+    </div>
   )
 }
