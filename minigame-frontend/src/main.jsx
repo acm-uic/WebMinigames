59412a123436f5fefdcafd908fdacff639851196
AdamAubs <97141419+AdamAubs@users.noreply.github.com>
2024-10-20 21:22:31 -0500

    Updated BrowserRouter syntax

--- minigame-frontend/src/main.jsx
+++ minigame-frontend/src/main.jsx
@@ -1,24 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./page/Layout.jsx";
+import { AdrianContributor } from "./contributors/Contributors";
+import Homepage from "./page/Homepage.jsx";
+import AllGames from "./page/AllGames.jsx";
+import Profile from "./page/Profile.jsx";
 
-import App from './App.jsx'
-import './index.css'
-import { Homepage } from './page/Homepage.jsx';
+import "./index.css";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />
-  },
-  {
-    path: "/home",
-    element: <Homepage />
-  }
-])
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="contributors" element={<AdrianContributor />} />
+          <Route index element={<Homepage />} />
+          <Route path="all-games" element={<AllGames />} />
+          <Route path="profile" element={<Profile />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
