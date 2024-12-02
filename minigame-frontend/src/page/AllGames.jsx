aabd1e33cf492d259dbbc3f05b2d753f2a443963
Ajknight121 <aknig2@uic.edu>
2024-12-02 14:55:27 -0600

    Remove leaderboard from game page, game entries link to /game page

--- minigame-frontend/src/page/AllGames.jsx
+++ minigame-frontend/src/page/AllGames.jsx
@@ -1,15 +1,9 @@
-import React from "react";
-import Leaderboard from "../components/Leaderboard";
 
 const AllGames = () => {
-
-  const gameName = "EXAMPLE"
-  const player = {}
 
   return (
     <>
       <div>All Games Page</div>
-      <Leaderboard gameName={gameName} player={player}/>
     </>
   );
 };
