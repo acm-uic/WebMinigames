7059b8fe4cf82d78cd8bfa5854d38092420f47be
Chao Liu <laychao4397@gmail.com>
2025-02-03 22:58:44 -0600

    Add login modal and profile navigation logic

--- minigame-frontend/src/page/Contributors.jsx
+++ minigame-frontend/src/page/Contributors.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import { AdrianContributor } from '../contributors/Contributors'
+import React from "react";
+import {
+  AdrianContributor,
+  // JackContributor,
+} from "../contributors/Contributors";
 import "./contributors.css";
 export default function Contributors() {
   return (
-    <div className='contributors-page'>
-      <div className='contributors'>
+    <div className="contributors-page">
+      <div className="contributors">
         <AdrianContributor />
-
+        {/* <JackContributor /> */}
       </div>
     </div>
-  )
+  );
 }
