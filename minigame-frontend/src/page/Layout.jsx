8bdd0c764d5b7a94086ca13c03ba93c8126af7ce
Chao Liu <laychao4397@gmail.com>
2025-02-03 23:18:30 -0600

    Update Layout profile icon to link to profile page when logged in

--- minigame-frontend/src/page/Layout.jsx
+++ minigame-frontend/src/page/Layout.jsx
@@ -39,7 +39,23 @@ const Layout = () => {
           <div className="flex items-center space-x-4">
             <div className="text-white hover:text-gray-200">
               {/* If user is not logged in, show profile icon */}
-              {!isLoggedIn && (
+              {isLoggedIn ? (
+                <Link to="/profile" className="text-white hover:text-gray-200">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-8 h-8">
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                    />
+                  </svg>
+                </Link>
+              ) : (
                 <div onClick={handleProfileClick} className="cursor-pointer">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
