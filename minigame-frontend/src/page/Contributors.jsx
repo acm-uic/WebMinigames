52a231f6e508697ca0cf8b07da1a9d8aa98c14e0
Chao Liu <laychao4397@gmail.com>
2025-02-12 20:22:47 -0600

    Remove commented part of adding contributor

--- minigame-frontend/src/page/Contributors.jsx
+++ minigame-frontend/src/page/Contributors.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {
   AdrianContributor,
-  // JackContributor,
+  JackContributor,
 } from "../contributors/Contributors";
 import "./contributors.css";
 export default function Contributors() {
@@ -9,7 +9,7 @@ export default function Contributors() {
     <div className="contributors-page">
       <div className="contributors">
         <AdrianContributor />
-        {/* <JackContributor /> */}
+        <JackContributor />
       </div>
     </div>
   );
