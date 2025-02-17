b7452086d1eb2d27c2623bc6d4dfeb3143c7bcde
tdcoleman127 <tdcoleman127@github.com>
2025-02-16 22:11:49 -0600

    Created prototype of signed in profile for profileBio component

--- minigame-frontend/src/page/Profile.jsx
+++ minigame-frontend/src/page/Profile.jsx
@@ -1,6 +1,21 @@
-import React from "react";
+import React from 'react'
+import { ProfileBioComponent } from "../components/ProfileBioComponent.jsx";
+/* A next step can be making a default card */
 
-const Profile = () => {
-  return <div>Profile Page</div>;
-};
-export default Profile;
+export default function Profile() {
+
+  return (
+  <div>
+    <h1>Profile Page</h1>
+
+    <ProfileBioComponent
+      imageLink = {"https://i.pinimg.com/736x/c6/25/f6/c625f6315130d8329a95ae27d8e95564.jpg"}
+      /*Usernames have a 12 character limit to not trigger overflow*/
+      user = {"LaserGhost99"}
+      bio = {"Greetings! I enjoy playing Fortnite, Mario Kart, and Minecraft."}
+      isSignedIn = {true}
+    />
+    
+  </div>
+  )
+}
