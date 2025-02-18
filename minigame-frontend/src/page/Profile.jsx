72a865a042ae2a8e63c1804e7fd195f83044f09d
Ajknight121 <aknig2@uic.edu>
2025-02-18 17:43:59 -0600

    Start user context with example

--- minigame-frontend/src/page/Profile.jsx
+++ minigame-frontend/src/page/Profile.jsx
@@ -1,6 +1,13 @@
 import React from "react";
+import { UserContext } from "../domain/UserContext";
+import { useContext } from "react";
 
 const Profile = () => {
-  return <div>Profile Page</div>;
+  const {username, setUsername} = useContext(UserContext);
+  return <div>Profile Page
+    {username}
+
+    <button onClick={() => setUsername("Hello Username")}>Change username</button>
+  </div>;
 };
 export default Profile;
