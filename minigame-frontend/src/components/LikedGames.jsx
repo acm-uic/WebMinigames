664786adb5d6d3c3bbb95206f3eea2c570614f9d
Daniel Barajas <danbarajas7@gmail.com>
2025-02-18 10:02:33 -0600

    Implemented LIkedGames component

---
+++ minigame-frontend/src/components/LikedGames.jsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom"
+
+const LikedGames = ({games}) => {
+    if (!games) {
+        /* Example data if no games are provided */
+        games = [
+            {name: "Game 1", link: "/game"}, 
+            {name: "Game 2", link: "/game"}, 
+            {name: "Game 3", link: "/game"}, 
+            {name: "Game 4", link: "/game"},
+            {name: "Game 5", link: "/game"}
+        ];
+    }
+    
+    return (
+        <div className="w-full p-6 bg-zinc-100 text-xl">
+            <h1 className="mb-4">Liked Games</h1>
+            <ul className="w-full flex flex-nowrap gap-3 overflow-scroll hide-scrollbar">
+                {games.map(g => (
+                    <Link to={g.link}>
+                        <li key={g.name} className="min-w-32 w-32 h-20 bg-zinc-300 p-3 items-center justify-center flex text-center">
+                            {g.name}
+                        </li>
+                    </Link>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default LikedGames;
