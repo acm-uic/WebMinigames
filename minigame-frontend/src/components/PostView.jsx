502aced5731af7dd3f398f88fba4b34d4ffbe34f
Daniel Barajas <danbarajas7@gmail.com>
2025-03-18 15:05:27 -0500

    Added link to poster profile

--- minigame-frontend/src/components/PostView.jsx
+++ minigame-frontend/src/components/PostView.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 import { useState } from "react"
 
 const PostView = ({ username="Guest", icon="https://t4.ftcdn.net/jpg/02/15/84/43/240_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg", profile="/profile", body="Nothing here...", imgs }) => {
@@ -30,10 +31,12 @@ const PostView = ({ username="Guest", icon="https://t4.ftcdn.net/jpg/02/15/84/43
         <div className="w-1/2 p-6 bg-zinc-200 m-auto flex flex-col gap-10">
             {/* Username and Title Row */}
             <div className="flex gap-7 h-[50px]">
-                <div className="flex items-center gap-2 text-wrap w-[200px]">
-                    <img className="w-[50px] rounded-full" src={icon} />
-                    {username}
-                </div>
+                <Link to={profile}>
+                    <div className="flex items-center gap-2 text-wrap w-[200px]">
+                        <img className="w-[50px] rounded-full" src={icon} />
+                        {username}
+                    </div>
+                </Link>
 
                 <div className="flex items-center">
                     <h1 className="text-[24px]">Example Title</h1>
