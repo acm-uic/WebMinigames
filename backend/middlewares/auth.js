1448103f9f65c449d9e2b861a1f588f29be93894
AlgoriThai07 <thaiviet0703@gmail.com>
2025-03-24 22:40:42 -0500

    rewrite auth.js

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,31 +1,73 @@
-import jwt from "jsonwebtoken";
-import { SECRET_KEY } from "../config.js";
+import { verifyToken } from "../utils/token.js";
 
 const AuthMiddlewares = {
-  validateToken: (req, res, next) => {
-    const authHeader = req.headers["authorization"];
-    if (authHeader) {
+  verifyAccessToken: (req, res, next) => {
+    try {
+      const authHeader = req.headers["authorization"];
+      if (!authHeader) throw new Error("Please enter token");
       // Split the access token from the bearer token
       const token = authHeader.split(" ")[1];
 
       // Validate the access token
-      jwt.verify(token, SECRET_KEY, (err, decoded) => {
-        if (err) {
-          res.status(401).send({
-            message: "Access token is invalid",
-            success: false,
-            data: null,
-          });
-        } else {
-          // Save user info into req,
-          // Req will be transferred to next handlers
-          req.user = decoded;
-          return next();
-        }
-      });
-    } else {
+      const data = verifyToken(token, "AT");
+      req.user = data;
+      return next();
+    } catch (error) {
+      let type = "";
+      let getMessage = "";
+      switch (error.message) {
+        case "invalid signature":
+          getMessage = "Cannot verify token";
+          type = "INVALID_TOKEN";
+          break;
+        case "jwt expired":
+          getMessage = "Token is expired";
+          type = "EXP_TOKEN";
+          break;
+        default:
+          getMessage = "Cannot authenticate user";
+          type = "UNAUTH";
+          break;
+      }
       res.status(401).send({
-        message: "Access token is missing",
+        message: getMessage,
+        type,
+        success: false,
+        data: null,
+      });
+    }
+  },
+  verifyRefreshToken: (req, res, next) => {
+    try {
+      const authHeader = req.headers["authorization"];
+      if (!authHeader) throw new Error("Please enter token");
+      // Split the access token from the bearer token
+      const token = authHeader.split(" ")[1];
+
+      // Validate the access token
+      const data = verifyToken(token, "RT");
+      req.user = data;
+      return next();
+    } catch (error) {
+      let type = "";
+      let getMessage = "";
+      switch (error.message) {
+        case "invalid signature":
+          getMessage = "Cannot verify token";
+          type = "INVALID_TOKEN";
+          break;
+        case "jwt expired":
+          getMessage = "Token is expired";
+          type = "EXP_TOKEN";
+          break;
+        default:
+          getMessage = "Cannot authenticate user";
+          type = "UNAUTH";
+          break;
+      }
+      res.status(401).send({
+        message: getMessage,
+        type,
         success: false,
         data: null,
       });
