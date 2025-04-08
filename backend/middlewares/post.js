b247e3c8615adef6439c63d791b9ca1b95e44711
AlgoriThai07 <thaiviet0703@gmail.com>
2025-04-07 20:33:02 -0500

    create post update middleware

--- backend/middlewares/post.js
+++ backend/middlewares/post.js
@@ -17,6 +17,26 @@ const PostMiddlewares = {
       });
     }
   },
+  // A middleware for update post
+  updatePost: (req, res, next) => {
+    try {
+      const { title, body } = req.body;
+      const listFile = req.files;
+      // Throw error if the user doesn't update anything
+      if (!title || !body || !listFile) {
+        throw new Error("Please enter an updated field!");
+      }
+
+      return next();
+    } catch (error) {
+      res.status(400).send({
+        message: error.message,
+        success: false,
+        data: null,
+        error,
+      });
+    }
+  },
 };
 
 export default PostMiddlewares;
