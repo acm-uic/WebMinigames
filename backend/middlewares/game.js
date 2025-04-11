cf0c2ee4ae90324fa55f662709654c5701444be9
AlgoriThai07 <thaiviet0703@gmail.com>
2025-04-10 22:40:42 -0500

    create middleware for creating games

---
+++ backend/middlewares/game.js
@@ -0,0 +1,23 @@
+const GameMiddlewares = {
+  createGame: (req, res, next) => {
+    try {
+      const { gameName, description } = req.body;
+
+      console.log(req.body);
+      if (!gameName) throw new Error("Please enter gameName");
+
+      if (!description) throw new Error("Please enter game description");
+
+      return next();
+    } catch (error) {
+      res.status(400).send({
+        message: error.message,
+        success: false,
+        data: null,
+        error,
+      });
+    }
+  },
+};
+
+export default GameMiddlewares;
