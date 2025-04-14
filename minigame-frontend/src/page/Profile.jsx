157493561c4aa4760acd6081e13da5d5b2318831
Ajknight121 <97642775+Ajknight121@users.noreply.github.com>
2025-04-14 15:47:54 -0500

    edit profile view

--- minigame-frontend/src/page/Profile.jsx
+++ minigame-frontend/src/page/Profile.jsx
@@ -1,19 +1,33 @@
 import React from 'react'
 import { UserContext } from "../domain/UserContext";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { ProfileBioComponent } from "../components/ProfileBioComponent.jsx";
 import { ProfileDetails } from "../components/ProfileDetails.jsx";
 import "./profile.css";
 
 export default function Profile() {
-  const { username, setUsername } = useContext(UserContext);
+  const { username, profileIcon, aboutMe, updateProfile,isLoggedIn } = useContext(UserContext);
+  const [editing, setEditing] = useState(false);
+  const updateBio = (username, bio, image) => {
+    console.log("updating profile");
+    updateProfile(username, bio, image)
+    .then((res) => res == true ? handleEdit() : console.log("failed to update profile"));
+  }
+
+  const handleEdit = () => {
+    setEditing((prev)=> !prev);
+  }
+
   return (
     <div>
       <ProfileBioComponent
-        imageLink={"https://i.pinimg.com/736x/c6/25/f6/c625f6315130d8329a95ae27d8e95564.jpg"}
-        user={"LaserGhost99"}
-        bio={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
-        isSignedIn={true}
+        imageLink={profileIcon}
+        user={username}
+        bio={aboutMe}
+        handleEdit={handleEdit}
+        canEdit={isLoggedIn}
+        editing={editing}
+        updateBio={updateBio}
       />
       <ProfileDetails
         favorites={["Fortnite", "Smash", "DBZ", "Halo", "Pokemon", "Warhammer"]}
