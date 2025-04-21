6decad07a35038d4daa3f1343507b22cb1cb6b81
mike6612 <michaeljaimes66@gmail.com>
2025-04-21 00:13:42 -0500

    updating profile icon update implementation

--- minigame-frontend/src/components/ProfileBioComponent.jsx
+++ minigame-frontend/src/components/ProfileBioComponent.jsx
@@ -2,57 +2,98 @@ import "./css/ProfileBio.css";
 import { useEffect, useState } from "react";
 
 export function ProfileBioComponent(props) {
-    const { imageLink, user, bio,handleEdit, updateBio, canEdit, editing } = props;
-    const [newImageLink, setNewImageLink] = useState(imageLink);
-    const [newUser, setnewUser] = useState(user);
-    const [newBio, setNewBio] = useState(bio);
-    useEffect(()=> {
-        if (!editing) {
-            setNewImageLink(imageLink);
-            setNewBio(bio);
-            setnewUser(user);
-        }
-    }, [user, bio, imageLink])
-
+  const { imageLink, user, bio, handleEdit, updateBio, canEdit, editing } =
+    props;
+  const [newImageLink, setNewImageLink] = useState(imageLink);
+  const [newUser, setnewUser] = useState(user);
+  const [newBio, setNewBio] = useState(bio);
+  useEffect(() => {
     if (!editing) {
-        return (
-            <div className="user-card">
-                <div>
-                    {canEdit ? <button className="button" onClick={handleEdit}><img className="editButton"
-                        src="https://cdn-icons-png.flaticon.com/512/4226/4226577.png"
-                        alt="Edit icon" /></button> : ""}
-                </div>
-                <img className="profilePicture" src={imageLink}
-                    alt="Profile Picture" />
-    
-                    <h1 className="username">{user}</h1>
-                <h2 className="bio">biography</h2>
-                <br></br>
-                <p className="description">{bio}</p>
-                <br></br>
-            </div>
-        );
-    } else {
-        return (
-            <div className="user-card">
-                <div>
-                    <button className="button" onClick={() => updateBio(newUser, newBio, newImageLink)}><img className="editButton"
-                        src="https://cdn-icons-png.flaticon.com/512/4226/4226577.png"
-                        alt="Edit icon" /></button>
-                </div>
-                <img className="profilePicture" src={newImageLink} alt="Profile Picture" />
-                <input className="edit" type="text" value={newImageLink} onChange={() => setNewImageLink(event.target.value)}/>
-                <h1 className="username">{user}</h1>
-                <input className="username edit" type="text" value={newUser} onChange={() => setnewUser(event.target.value)}/>
-                <h2 className="bio">biography</h2>
-                <br></br>
-                <p className="description">{bio}</p>
-                <textarea className="description edit" value={newBio} onChange={() => setNewBio(event.target.value)} />
-                <br></br>
-            </div>
-        )
+      setNewImageLink(imageLink);
+      setNewBio(bio);
+      setnewUser(user);
     }
-    
+  }, [user, bio, imageLink]);
+
+  if (!editing) {
+    return (
+      <div className="user-card">
+        <div>
+          {!canEdit ? (
+            <button className="button" onClick={handleEdit}>
+              <img
+                className="editButton"
+                src="https://cdn-icons-png.flaticon.com/512/4226/4226577.png"
+                alt="Edit icon"
+              />
+            </button>
+          ) : (
+            ""
+          )}
+        </div>
+        <img className="profilePicture" src={imageLink} alt="Profile Picture" />
+
+        <h1 className="username">{user}</h1>
+        <h2 className="bio">biography</h2>
+        <br></br>
+        <p className="description">{bio}</p>
+        <br></br>
+      </div>
+    );
+  } else {
+    return (
+      <div className="user-card">
+        <div>
+          <button
+            className="button"
+            onClick={() => updateBio(newUser, newBio, newImageLink)}
+          >
+            <img
+              className="editButton"
+              src="https://cdn-icons-png.flaticon.com/512/4226/4226577.png"
+              alt="Edit icon"
+            />
+          </button>
+        </div>
+        <div className="flex items-center justify-center relative group bg-green-200">
+          <img
+            className=" bg-green-400 profilePicture "
+            src={newImageLink}
+            alt="Profile Picture"
+          />
+          <div className="absolute left-[18.5%] h-[100%] w-[63%] inset-0  bg-black bg-opacity-40 flex items-end justify-center rounded-full opacity-0 group-hover:opacity-100 transition-opacity duration-200">
+            <div className="text-white text-xs mb-2 flex items-center gap-1">
+              Edit
+            </div>
+          </div>
+        </div>
+
+        {/* <input
+          className="edit"
+          type="text"
+          value={newImageLink}
+          onChange={() => setNewImageLink(event.target.value)}
+        /> */}
+
+        <h1 className="username">{user}</h1>
+        <input
+          className="username edit"
+          type="text"
+          value={newUser}
+          onChange={() => setnewUser(event.target.value)}
+        />
+        <h2 className="bio">biography</h2>
+        <br></br>
+        <p className="description">{bio}</p>
+        <textarea
+          className="description edit"
+          value={newBio}
+          onChange={() => setNewBio(event.target.value)}
+        />
+        <br></br>
+      </div>
+    );
+  }
 }
 
 // ProfileBioComponent.defaultProps = {
@@ -61,3 +102,4 @@ export function ProfileBioComponent(props) {
 //     bio: "asdadasadasfasdfasdaswerfwfwferrefwerewrwerererwrewercaddcapocmevimoasmv aoimacviocsdasdrandom paragraphs can be an excellent way",
 //     isSignedIn: false
 // }
+
