112a73456b2dde366a048368fef736b20a6ccbf4
AlgoriThai07 <thaiviet0703@gmail.com>
2025-04-21 11:29:34 -0500

    create a public route for posts

--- backend/routes/public.js
+++ backend/routes/public.js
@@ -2,10 +2,13 @@ import { Router } from "express";
 import UserPublicRoute from "./public/user.js";
 import GamePublicRoute from "./public/game.js";
 import CommentPublicRoute from "./public/comment.js";
+import PostPublicRoute from "./public/post.js";
 
 const PublicRoute = Router();
 // User route
 PublicRoute.use("/users", UserPublicRoute);
+// Post route
+PublicRoute.use("/posts", PostPublicRoute);
 // Comment route
 PublicRoute.use("/comments", CommentPublicRoute);
 // Game route
---
+++ backend/routes/public/post.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import PostMiddlewares from "../../middlewares/post.js";
+import PostControllers from "../../controllers/post.js";
+
+const PostPublicRoute = Router();
+
+// Get all posts
+PostPublicRoute.get("/get");
+
+export default PostPublicRoute;
