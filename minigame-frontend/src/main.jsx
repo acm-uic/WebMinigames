1888773e37f4500ec09c44336faa9122f1bfa24d
Daniel Barajas <danbarajas7@gmail.com>
2025-04-22 15:01:24 -0500

    Added post page to route

--- minigame-frontend/src/main.jsx
+++ minigame-frontend/src/main.jsx
@@ -13,6 +13,7 @@ import "./index.css";
 import Contributors from "./page/Contributors.jsx";
 import GameDetails from "./components/GameDetails.jsx";
 import PostView from "./components/PostView.jsx";
+import Post from "./page/Post.jsx";
 import PostViewPage from "./page/PostViewPage.jsx";
 
 const exampleGame = {
@@ -46,6 +47,7 @@ export default function App() {
               path="game-details"
               element={<GameDetails game={exampleGame} />}
             />
+            <Route path="post" element={<Post Post={<PostView imgs={["https://steamuserimages-a.akamaihd.net/ugc/273968969351308719/A642CC447714B0B1F2F927C1738A9C262308E7B0/?imw=128&imh=128&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=true", "https://t4.ftcdn.net/jpg/02/15/84/43/240_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg"]} />} />} />
             <Route path="create-post" element={<PostViewPage />} />
           </Route>
         </Routes>
