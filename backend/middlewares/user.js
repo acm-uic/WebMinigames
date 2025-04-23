5abc71110121ac7daa2667f2650aad155fd91017
AlgoriThai07 <thaiviet0703@gmail.com>
2025-04-22 21:38:54 -0500

    Middleware for unliking game and removing interest

--- backend/middlewares/user.js
+++ backend/middlewares/user.js
@@ -87,7 +87,39 @@ const UserMiddlewares = {
       });
     }
   },
+  unlikeGame: (req, res, next) => {
+    try {
+      // Get the game name
+      const { game } = req.body;
+      // Give an error if missing any information
+      if (!game) throw new Error("Please enter game!");
+
+      return next();
+    } catch (error) {
+      res.status(400).send({
+        message: error.message,
+        success: false,
+        data: null,
+      });
+    }
+  },
   addInterest: (req, res, next) => {
+    try {
+      // Get the interest
+      const { interest } = req.body;
+      // Give an error if missing any information
+      if (!interest) throw new Error("Please enter interest!");
+
+      return next();
+    } catch (error) {
+      res.status(400).send({
+        message: error.message,
+        success: false,
+        data: null,
+      });
+    }
+  },
+  removeInterest: (req, res, next) => {
     try {
       // Get the interest
       const { interest } = req.body;
